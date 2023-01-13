65f040770c7c051e9351a7924d837e48d2371fb0
yeongroke <chfaos120@naver.com>
2023-01-14 00:13:27 +0900

    config: gateway Common Filter add

---
+++ gateway/src/main/java/com/yrkim/gateway/config/LoggingFilter.java
@@ -0,0 +1,44 @@
+package com.yrkim.gateway.config;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+    public LoggingFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+
+        return new OrderedGatewayFilter((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+            log.info("Logging Filter baseMessage: -> {}", config.getBaseMessage());
+
+            if (config.isPreLogger()) log.info("Logging Filter Start: request id -> {}", request.getId());
+            //Custom Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger())
+                    log.info("Logging Filter End: response status code -> {}", response.getStatusCode());
+            }));
+        }, Ordered.LOWEST_PRECEDENCE);//제일 낮은 순위로 필터를 적용
+    }
+
+    @Data
+    public static class Config{
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
